eafaf3b5beb179d6143cb299f100a75d2b980a3e
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-13 15:48:31 -0300

    Array acessando e caculando dados do array react JSX

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import React from 'react';
 
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebok', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebok', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
 const App = () => {
-  const radom = Math.random();
-
-  function mostraNome(sobrenome) {
-    return 'Victor ' + sobrenome;
-  }
-
-  const carro = {
-    nome: 'Astra',
-    rodas: '4',
-  };
-
-  const estiloP = {
-    color: "blue",
-    fontSize: '2rem',
-
-  }
+  const dados = luana;
+  const total = dados.compras
+    .map((item) => Number(item.preco.replace('R$', '')))
+    .reduce((a, b) => a + b);
 
   return (
-    <>
-      <p>{radom * 100}</p>
-      <p>{true ? 'auuu' : 'auwaa'}- {10}</p>
-      <p style={estiloP}>{new Date().getFullYear()}</p>
-      <p>{carro.nome}</p>
-      <p>{carro.rodas}</p>
-      {mostraNome('Albuquerque')}
-    </>
-  );
+    <di>
+      <p>Nome: {dados.cliente}</p>
+      <p>idade: {dados.idade}</p>
+      <p>
+        Situação: {' '}
+        <span style={{ color: dados.ativa ? 'green' : 'red' }}>{dados.ativa ? 'Ativa' : 'Inativa'}</span>
+      </p>
+      <p>Total: {total}</p>
+      {total > 10000 && <p>Você está gastando muito</p>}
+    </di>
+  )
 };
 
 
