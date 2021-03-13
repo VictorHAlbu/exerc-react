32d3d7ca1f31f4f6e459429c664ada46d0e001d0
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-03-13 15:12:47 -0300

    usando javascript e estilos direto no react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const radom = Math.random();
+
+  function mostraNome(sobrenome) {
+    return 'Victor ' + sobrenome;
+  }
+
+  const carro = {
+    nome: 'Astra',
+    rodas: '4',
+  };
+
+  const estiloP = {
+    color: "blue",
+    fontSize: '2rem',
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{radom * 100}</p>
+      <p>{true ? 'auuu' : 'auwaa'}- {10}</p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p>{carro.nome}</p>
+      <p>{carro.rodas}</p>
+      {mostraNome('Albuquerque')}
+    </>
   );
-}
+};
+
 
 export default App;
